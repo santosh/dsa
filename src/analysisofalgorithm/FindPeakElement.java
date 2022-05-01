4e271ee8a1bf1dbbeaf1a6529a84ff1a443e16cd
Santosh Kumar <sntshkmr60@gmail.com>
2022-05-01 23:24:10 +0530

    findThePeak O(logn)

--- src/analysisofalgorithm/FindPeakElement.java
+++ src/analysisofalgorithm/FindPeakElement.java
@@ -8,8 +8,17 @@ import java.util.regex.*;
 
 public class FindPeakElement {
     public static int findThePeak(int[] arr) {
-        for (int i = 1; i < arr.length-1; i++) {
-            if (arr[i-1] < arr[i] && arr[i+1] < arr[i]) return arr[i];
+        int n = arr.length;
+        if (n == 1)
+            return 0;
+        if (arr[0] >= arr[1])
+            return 0;
+        if (arr[n - 1] >= arr[n - 2])
+            return arr[n - 1];
+        for(int i = 1; i < n - 1; i++) {
+            if (arr[i] >= arr[i - 1] &&
+                    arr[i] >= arr[i + 1])
+                return arr[i];
         }
         return -1;
     }
