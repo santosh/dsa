01f8381255b79e706a2c8911d07a15f941020674
Santosh Kumar <sntshkmr60@gmail.com>
2022-05-29 13:21:21 +0530

    Add singly ListNode

---
+++ src/linkedlist/ListNode.java
@@ -0,0 +1,34 @@
+package linkedlist;
+
+public class ListNode {
+    private int data;
+    private ListNode next;
+
+    public ListNode(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ListNode(" +
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
