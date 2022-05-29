c02f12579b34dbe04bff6636e47b750fac3cc226
Santosh Kumar <sntshkmr60@gmail.com>
2022-05-29 11:24:24 +0530

    Frequency of each element

---
+++ src/searchingandsorting/FrequencyOfEachElement.java
@@ -0,0 +1,39 @@
+package searchingandsorting;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of length n, where all the element are between 1 and n.
+ * Find the frequency of all the elements.
+ *
+ * 1 <= arr[i] <= n
+ */
+public class FrequencyOfEachElement {
+    public static void findFrequency(int[] arr) {
+        // Bring all the elements in the range of [0, arr.length -1]
+
+        for(int i=0;i<arr.length;i++){
+            arr[i]=arr[i]-1;
+        }
+
+        // Transformation of the array
+        for(int i=0;i<arr.length;i++){
+            arr[arr[i]%arr.length] = arr[arr[i]%arr.length]+arr.length;
+        }
+
+        // Find the frequency
+        for(int i=0;i<arr.length;i++){
+            arr[i]=arr[i]/arr.length;
+        }
+
+        System.out.println(Arrays.toString(arr));
+        for (int i=0 ;i<arr.length;i++){
+            System.out.println((i+1) +" -> "+arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 1, 4, 5};
+        findFrequency(arr);
+    }
+}
