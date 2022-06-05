e7c825ca5d8ee51bba7b67b4f94e80f4e7509812
Santosh Kumar <sntshkmr60@gmail.com>
2022-06-06 00:25:52 +0530

    Recursive Bubble Sort

--- src/sort/Bubble.java
+++ src/sort/Bubble.java
@@ -3,6 +3,7 @@ package sort;
 import java.util.Arrays;
 
 public class Bubble {
+    static boolean isSorted = false;
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i <= arr.length-1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
@@ -32,13 +33,49 @@ public class Bubble {
         }
     }
 
+    public static void bubbleSortRecursive(int[] arr, int n) {
+        if (n<=1) {
+            return;
+        }
+
+
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+        bubbleSortRecursive(arr, n-1);
+    }
+
+    public static void enhancedBubbleSortRecursive(int[] arr, int n) {
+        if (n<=1 || isSorted) {
+            return;
+        }
+
+        isSorted = true;
+        for (int j = 0; j < arr.length - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                isSorted = false;
+            }
+        }
+
+        bubbleSortRecursive(arr, n-1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
 
+//        System.out.println(Arrays.toString(arr));
+//        bubbleSort(arr);
+//        System.out.println(Arrays.toString(arr));
         System.out.println(Arrays.toString(arr));
-
-        bubbleSort(arr);
-
+        enhancedBubbleSortRecursive(arr, arr.length);
         System.out.println(Arrays.toString(arr));
     }
 }
@@ -46,5 +83,7 @@ public class Bubble {
 // Stable: Yes
 // Worst Time Complexity: O(n^2)
 // Best Time Complexity: O(n^2)
+// Number of comparison: O(n^2)
+// Number of swap: O(n^2)
 // Remark: Modified bubble sort can be used  for nearly sorted array.
 //         Modified bubble sort can also be used to check if an array is sorted.
