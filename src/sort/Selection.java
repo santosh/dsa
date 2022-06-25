9a94bdd89efc0ee1012b859559edc893eb09510d
Santosh Kumar <sntshkmr60@gmail.com>
2022-06-25 20:35:08 +0530

    Selection Sort Linked List

--- src/sort/Selection.java
+++ src/sort/Selection.java
@@ -1,5 +1,8 @@
 package sort;
 
+import linkedlist.DLLNode;
+import linkedlist.DoublyLinkedList;
+
 import java.util.Arrays;
 
 public class Selection {
@@ -28,6 +31,55 @@ public class Selection {
         selectionSort(arr);
 
         System.out.println(Arrays.toString(arr));
+    }
+}
+
+class SelectionSortDLL {
+
+    public static DLLNode selectionSort(DLLNode head)
+    {
+        DLLNode temp = head;
+
+        while (temp!=null) {
+            DLLNode min = temp;
+            DLLNode r = temp.getNext();
+
+            // Traverse the unsorted sublist
+            while (r!=null) {
+                if (min.getData() > r.getData())
+                    min = r;
+
+                r = r.getNext();
+            }
+
+            int x = temp.getData();
+            temp.setData(min.getData());
+            min.setData(x);
+            temp = temp.getNext();
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        DLLNode node1 = new DLLNode(3);
+        DLLNode node2 = new DLLNode(2);
+        DLLNode node3 = new DLLNode(1);
+        DLLNode node4 = new DLLNode(5);
+        DLLNode node5 = new DLLNode(4);
+
+        node1.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
+        node4.setNext(node5);
+
+        node2.setPrev(node1);
+        node3.setPrev(node2);
+        node4.setPrev(node3);
+        node5.setPrev(node4);
+
+        DoublyLinkedList.traverse(node1);
+
+        DoublyLinkedList.traverse(selectionSort(node1));
     }
 }
 
