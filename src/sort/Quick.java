c146aab7795df616095db6e5d61df04ae423c0be
Santosh Kumar <sntshkmr60@gmail.com>
2022-06-26 00:30:13 +0530

    Add QuickSort Linked List

--- src/sort/Quick.java
+++ src/sort/Quick.java
@@ -1,5 +1,8 @@
 package sort;
 
+import linkedlist.LinkedList;
+import linkedlist.ListNode;
+
 import java.util.Arrays;
 import java.util.Random;
 
@@ -66,4 +69,73 @@ public class Quick {
         quickSort(arr, 0, arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
+}
+
+
+class QuickSortLinkedList {
+
+
+    static ListNode partitionLast(ListNode start, ListNode end)
+    {
+        if (start == end || start == null || end == null)
+            return start;
+
+        ListNode pivot_prev = start;
+        ListNode curr = start;
+        int pivot = end.getData();
+
+        while (start != end) {
+            if (start.getData() < pivot) {
+                pivot_prev = curr;
+                int temp = curr.getData();
+                curr.setData(start.getData());
+                start.setData(temp);
+                curr = curr.getNext();
+            }
+            start = start.getNext();
+        }
+
+        int temp = curr.getData();
+        curr.setData(pivot);
+        end.setData(temp);
+
+        return pivot_prev;
+    }
+
+    static void quickSort(ListNode start, ListNode end)
+    {
+        if(start == null || start == end|| start == end.getNext() )
+            return;
+
+        ListNode pivot_prev = partitionLast(start, end);
+        quickSort(start, pivot_prev);
+
+        if (pivot_prev != null && pivot_prev == start)
+            quickSort(pivot_prev.getNext(), end);
+
+        else if (pivot_prev != null
+                && pivot_prev.getNext() != null)
+            quickSort(pivot_prev.getNext().getNext(), end);
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(5);
+        ListNode node2 = new ListNode(3);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(4);
+
+        head.setNext(node2);
+        node2.setNext(node3);
+        node3.setNext(node4);
+        node4.setNext(node5);
+
+        LinkedList.traverse(head);
+
+        quickSort(head,node5);
+        LinkedList.traverse(head);
+
+
+    }
+
 }
